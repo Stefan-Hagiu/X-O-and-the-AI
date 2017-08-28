c27a4312c72afecff7bc2283f6f3e20a1600d156
Stefan Hagiu <hagiu.stefan@yahoo.com>
2017-08-28 17:47:20 +0300

    Added buttons to tic-tac-toe game menu

--- gameMenu.py
+++ gameMenu.py
@@ -1,5 +1,12 @@
 from windowTemplate import *
 
 class GameMenu(WindowTemplate):
+
     def __init__(self, programWindow):
         super(GameMenu, self).__init__(programWindow)
+
+    def loadUI(self):
+        self.buttonList = [[Button(self._thisWindow) for i in range (0, 4)] for j in range (0, 4)]
+        for i in range (1, 4):
+            for j in range (1, 4):
+                self.buttonList[i][j].grid(row=i, column=j)
