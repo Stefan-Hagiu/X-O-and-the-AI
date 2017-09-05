e4192ffea4707048a5e6b4f2e27bb007652a08fe
Stefan Hagiu <hagiu.stefan@yahoo.com>
2017-09-05 15:51:01 +0300

    Added AITrainer source file

---
+++ AITrainer.py
@@ -0,0 +1,10 @@
+from ticTacToe import ticTacToe
+from neuralNetwork import neuralNetwork
+
+class AITrainer:
+    def __init__(self, numberOfAIs):
+        self.AIList = []
+        self.numberOfAIs = numberOfAIs
+
+    def crank(self):
+
