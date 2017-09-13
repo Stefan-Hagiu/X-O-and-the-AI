d6e5321a91a83c4f1a639ddb2019428e5321a212
Stefan Hagiu <hagiu.stefan@yahoo.com>
2017-09-13 12:25:54 +0300

    Added a couple buttons in vsAIMenu

--- vsAIMenu.py
+++ vsAIMenu.py
@@ -1,4 +1,4 @@
-from windowTemplate import WindowTemplate
+from windowTemplate import *
 from AITrainer import AITrainer
 
 class VsAIMenu(WindowTemplate):
@@ -7,4 +7,16 @@ class VsAIMenu(WindowTemplate):
         aITrainer = AITrainer(1000)
         
     def loadUI(self):
-        todo = 1
+        self.startTrainingButton = Button(self._thisWindow, text="Start training neural networks",
+                                          command=self.startTrainingNeuralNet())
+        self.startTrainingButton.grid(row = 0, column = 0)
+
+        self.stopTrainingButton = Button(self._thisWindow, text="Stop training neural networks", state=DISABLED,
+                                         command=self.stopTrainingNeuralNet())
+        self.stopTrainingButton.grid(row=1, column=0)
+
+    def startTrainingNeuralNet(self):
+        todo=1
+
+    def stopTrainingNeuralNet(self):
+        todo=1
