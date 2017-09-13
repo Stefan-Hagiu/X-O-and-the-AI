bbd9c6c0840b7646fa0525719ee55d88057998e3
Stefan Hagiu <hagiu.stefan@yahoo.com>
2017-09-13 14:39:30 +0300

    Buttons now working

--- vsAIMenu.py
+++ vsAIMenu.py
@@ -3,20 +3,24 @@ from AITrainer import AITrainer
 
 class VsAIMenu(WindowTemplate):
     def __init__(self, programWindow):
+
         super(VsAIMenu, self).__init__(programWindow)
-        aITrainer = AITrainer(1000)
+        self.aITrainer = AITrainer(1000)
         
     def loadUI(self):
-        self.startTrainingButton = Button(self._thisWindow, text="Start training neural networks",
-                                          command=self.startTrainingNeuralNet())
+        self.startTrainingButton = Button(self._thisWindow, text="Start training neural networks")
         self.startTrainingButton.grid(row = 0, column = 0)
 
-        self.stopTrainingButton = Button(self._thisWindow, text="Stop training neural networks", state=DISABLED,
-                                         command=self.stopTrainingNeuralNet())
+        self.stopTrainingButton = Button(self._thisWindow, text="Stop training neural networks", state=DISABLED)
         self.stopTrainingButton.grid(row=1, column=0)
 
-    def startTrainingNeuralNet(self):
-        todo=1
+        self.startTrainingButton.bind("<Button-1>", self.startTrainingNeuralNet)
+        self.stopTrainingButton.bind("<Button-1>", self.stopTrainingNeuralNet)
 
-    def stopTrainingNeuralNet(self):
-        todo=1
+    def startTrainingNeuralNet(self, event):
+        self.startTrainingButton.config(state=DISABLED)
+        self.stopTrainingButton.config(state=NORMAL)
+
+    def stopTrainingNeuralNet(self, event):
+        self.startTrainingButton.config(state=NORMAL)
+        self.stopTrainingButton.config(state=DISABLED)
